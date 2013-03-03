6eb4e05e98b25aa7cbb1de241e8b442830f37739
pavelkalinnikov <pavelkalinnikov@gmail.com>
2013-03-04 00:31:08 +0300

    Added usage

--- indexer/main.cpp
+++ indexer/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
 int main(int argc, char** argv) {
+    if (argc < 2) {
+        std::cout << "Usage: " << argv[0] << " <directory>\n";
+        return 0;
+    }
+
     std::cout << "Hello. This is the indexer program.\n";
 
     return 0;
