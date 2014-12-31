c509927bc41de5d768ead41f2fb879826ed13aa8
pavelkalinnikov <pavelkalinnikov@gmail.com>
2014-12-31 22:49:39 +0300

    [Util] Introduced program options.
    A new interface uses boost::program_options library.
    Implemented indexer's option parsing using the new interface.

--- indexer/main.cpp
+++ indexer/main.cpp
@@ -1,32 +1,73 @@
 #include "lister.h"
 
 #include <index/writer.h>
+#include <util/program_options.h>
 
 #include <algorithm>
-#include <iostream>
 
 using namespace NCodesearch;
 
-int main(int argc, char** argv) {
-    if (argc < 3) {
-        std::cout << "Usage: " << argv[0] << " <directory> <index>\n";
-        return 1;
+class TConfig : public TProgramOptions::IConfig {
+public:
+    unsigned NumOfPositionalOptions() const override {
+        return 0;
     }
 
-    TListerConfig listerConfig;
-    listerConfig.Verbose = true;
-    TLister lister(listerConfig);
+    std::string GetProgramDescription() const {
+        return "Standalone codesearch indexer.\n";
+    }
+
+    void InitOptions(po::options_description& opts, po::positional_options_description&) override {
+        opts.add_options()
+            ("path,p", po::value<std::string>()->default_value("."), "path to a root directory")
+            ("index,i", po::value<std::string>()->required(), "index path prefix")
+            ;
+
+        po::options_description listerOpts("Lister options");
+
+        listerOpts.add_options()
+            ("verbose,v", "use verbose mode")
+            ("recursive,r", po::value<bool>()->default_value(true), "traverse recursively")
+            ("hidden", "do not ignore hidden directories/files")
+            ("max-depth", po::value<int>()->default_value(-1), "max recursion depth")
+            ;
+
+        opts.add(listerOpts);
+    }
+
+    void ProcessVariables(const po::variables_map& variables) override {
+        ListerConfig.Verbose = variables.count("verbose");
+        ListerConfig.Recursive = variables["recursive"].as<bool>();
+        ListerConfig.IgnoreHidden = variables.count("hidden");
+
+        IndexWriterConfig.Verbose = variables.count("verbose");
+
+        DocsPath = variables["path"].as<std::string>();
+        IndexPath = variables["index"].as<std::string>();
+    }
+
+public:
+    TListerConfig ListerConfig;
+    TIndexWriterConfig IndexWriterConfig;
+    std::string DocsPath;
+    std::string IndexPath;
+};
+
+int main(int argc, char** argv) {
+    TProgramOptions options(argc, argv);
+    TConfig config;
+    if (!options.Parse(&config))
+        return 1;
+
+    TLister lister(config.ListerConfig);
     vector<string> docs;
-    lister.List(string(argv[1]), docs);
+    lister.List(config.DocsPath, docs);
     std::sort(docs.begin(), docs.end());
 
-    TIndexWriterConfig writerConfig;
-    writerConfig.Verbose = true;
-    TIndexWriter writer(writerConfig);
+    TIndexWriter writer(config.IndexWriterConfig);
 
-    string indexPath = argv[2];
-    string idxPath = indexPath + ".idx";
-    string datPath = indexPath + ".dat";
+    string idxPath = config.IndexPath + ".idx";
+    string datPath = config.IndexPath + ".dat";
     writer.Index(docs, idxPath.c_str(), datPath.c_str());
 
     return 0;
---
+++ util/program_options.h
@@ -0,0 +1,93 @@
+#pragma once
+
+#include <boost/program_options.hpp>
+
+#include <algorithm>
+#include <iostream>
+#include <memory>
+
+namespace NCodesearch {
+
+namespace po = boost::program_options;
+
+class TProgramOptions {
+public:
+    class IConfig;
+
+public:
+    TProgramOptions(int argc, const char* const* argv)
+        : argc(argc)
+        , argv(argv)
+    { /* no-op */ }
+
+    bool Parse(IConfig* config);
+
+private:
+    void PrintUsage(std::ostream& output, IConfig* config, const po::options_description& opts,
+            const po::positional_options_description& posOpts);
+
+private:
+    const int argc;
+    const char* const* argv;
+};
+
+class TProgramOptions::IConfig {
+public:
+    virtual ~IConfig() { /* no-op */ }
+
+    virtual unsigned NumOfPositionalOptions() const = 0;
+    virtual std::string GetProgramDescription() const = 0;
+    virtual void InitOptions(po::options_description& opts, po::positional_options_description& posOpts) = 0;
+    virtual void ProcessVariables(const po::variables_map& variables) = 0;
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+void TProgramOptions::PrintUsage(std::ostream& output, IConfig* config,
+        const po::options_description& opts, const po::positional_options_description& posOpts)
+{
+    output << "Usage: " << argv[0];
+
+    if (!opts.options().empty())
+        output << " [options]";
+    if (posOpts.max_total_count() > 0) {
+        unsigned count = std::min(posOpts.max_total_count(), config->NumOfPositionalOptions());
+        for (unsigned index = 0; index != count; ++index)
+            output << ' ' << posOpts.name_for_position(index);
+        if (posOpts.max_total_count() > count)
+            output << " ...";
+    }
+    output << '\n';
+    output << opts << std::endl;
+}
+
+bool TProgramOptions::Parse(IConfig* config) {
+    std::string appName(argv[0]);
+
+    po::options_description opts("General options");
+    opts.add_options()("help,?", "show help message");
+    po::positional_options_description posOpts;
+    config->InitOptions(opts, posOpts);
+
+    po::variables_map varMap;
+    try {
+        po::store(po::command_line_parser(argc, argv).options(opts).positional(posOpts).run(), varMap);
+        if (varMap.count("help")) {
+            std::cout << appName << '\n' << config->GetProgramDescription() << '\n';
+            PrintUsage(std::cout, config, opts, posOpts);
+            return false;
+        }
+        po::notify(varMap);
+    } catch (po::error& err) {
+        std::cerr << "ERROR: " << err.what() << "\n\n";
+        PrintUsage(std::cerr, config, opts, posOpts);
+        return false;
+    }
+
+    config->ProcessVariables(varMap);
+
+    return true;
+}
+
+} // namespace NCodesearch
+
