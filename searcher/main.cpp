618b7d4a6da65c0b94ed47bd8db6495f3f63faa2
pavelkalinnikov <pavelkalinnikov@gmail.com>
2015-01-02 15:31:20 +0300

    [Searcher] Switched to TProgramOptions.

--- searcher/main.cpp
+++ searcher/main.cpp
@@ -2,6 +2,7 @@
 #include "query.h"
 
 #include <base/types.h>
+#include <util/program_options.h>
 
 #include <fstream>
 #include <iostream>
@@ -11,22 +12,67 @@
 using namespace std;
 using namespace NCodesearch;
 
-int main(int argc, char** argv) {
-    if (argc < 4) {
-        cout << "Usage: " << argv[0] << " <index> <query> <regex>\n";
+class TConfig : public TProgramOptions::IConfig {
+public:
+    unsigned NumOfPositionalOptions() const override {
         return 1;
     }
 
-    TQueryTreeNode* query = TQueryFactory::Parse(argv[2]);
-    string indexPath = argv[1];
-    string idxPath = indexPath + ".idx";
-    string datPath = indexPath + ".dat";
-    TSearcherConfig config;
-    TSearcher searcher(config);
-    string regexp = argv[3];
-    if (regexp == "--")
-        regexp = argv[2];
-    searcher.Search(idxPath.c_str(), datPath.c_str(), query, cout, regexp.c_str());
+    std::string GetProgramDescription() const {
+        return "Standalone codesearch searcher.\n";
+    }
+
+    void InitOptions(po::options_description& opts, po::positional_options_description& posOpts) override {
+        opts.add_options()
+            ("regexp", po::value<std::string>()->required(), "query regexp")
+            ("index,i", po::value<std::string>()->required(), "index path prefix")
+            ("filter,f", po::value<std::string>()->default_value("++"), "trigram index filter query")
+            ;
+
+        po::options_description searcherOpts("Searcher options");
+        searcherOpts.add_options()
+            ("verbose,v", "use verbose mode")
+            ("color", po::value<bool>()->default_value(true), "use colored output")
+            ("line-number,n", "prefix matched lines with 1-base line number within a file")
+            ("files-with-matches,l", "suppress normal output by printing each file matching a trigram query")
+            ;
+
+        opts.add(searcherOpts);
+
+        posOpts.add("regexp", 1);
+    }
+
+    void ProcessVariables(const po::variables_map& variables) override {
+        SearcherConfig.Verbose = variables.count("verbose");
+        SearcherConfig.ColoredOutput = variables["color"].as<bool>();
+        SearcherConfig.PrintLineNumbers = variables.count("line-number");
+        SearcherConfig.JustFilter = variables.count("files-with-matches");
+
+        IndexPath = variables["index"].as<std::string>();
+        Regexp = variables["regexp"].as<std::string>();
+        Query = variables["filter"].as<std::string>();
+        if (Query == "++")
+            Query = Regexp;
+    }
+
+public:
+    TSearcherConfig SearcherConfig;
+    std::string IndexPath;
+    std::string Query;
+    std::string Regexp;
+};
+
+int main(int argc, char** argv) {
+    TProgramOptions options(argc, argv);
+    TConfig config;
+    if (!options.Parse(&config))
+        return 1;
+
+    TQueryTreeNode* query = TQueryFactory::Parse(config.Query);
+    string idxPath = config.IndexPath + ".idx";
+    string datPath = config.IndexPath + ".dat";
+    TSearcher searcher(config.SearcherConfig);
+    searcher.Search(idxPath.c_str(), datPath.c_str(), query, cout, config.Regexp.c_str());
 
     TQueryFactory::Free(query);
 
